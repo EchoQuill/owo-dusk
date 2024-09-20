6061d2d02df2cca62251cb32521de870549bb930
EchoQuill <safnaj059@gmail.com>
2024-09-20 20:35:15 +0530

    v1.6.0 - update version

--- uwu.py
+++ uwu.py
@@ -62,7 +62,7 @@ def resource_path(relative_path):
 with open(resource_path("config.json")) as file:
     config = json.load(file)
 #----------OTHER VARIABLES----------#
-version = "1.5.4"
+version = "1.6.0"
 offline = config["offlineStatus"]
 ver_check_url = "https://raw.githubusercontent.com/EchoQuill/owo-dusk/main/version.txt"
 quotesUrl = "https://favqs.com/api/qotd" #["https://thesimpsonsquoteapi.glitch.me/quotes", "https://favqs.com/api/qotd"]
