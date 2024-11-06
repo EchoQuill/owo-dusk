0696e2da9c3649985af069c15ed856e8fa0fa517
EchoQuill <safnaj059@gmail.com>
2024-11-06 19:18:49 +0530

    feat: add auto battle and fix hunt

--- cogs/battle.py
+++ cogs/battle.py
@@ -11,18 +11,19 @@ class Owo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.bot.log(f"conf2 - OwO","purple")
-
-    async def cog_load(self):
-        if not config_dict["commands"]["hunt"]["enabled"]:
-            await self.bot.unload_extension("cogs.giveaway")
     
+    async def cog_load(self):
+        if not config_dict["commands"]["battle"]["enabled"]:
+            await self.bot.unload_extension("cogs.giveaway")
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.channel.id == self.bot.cm.id and message.author.id == self.bot.owo_bot_id:
-            if 'you found:' in message.content.lower() or "caught" in message.content.lower():
-                await asyncio.sleep(self.bot.random_float(config_dict["commands"]["hunt"]["cooldown"]))
-                self.bot.queue.put("hunt")
-                
+            if message.embeds:
+                for embed in message.embeds:
+                    if embed.author.name is not None and "goes into battle!" in embed.author.name.lower():
+                        await asyncio.sleep(self.bot.random_float(config_dict["commands"]["hunt"]["cooldown"]))
+                        self.bot.queue.put("battle")
                 
                 
 
