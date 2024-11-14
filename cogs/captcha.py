2e1c1fadff840edbcfa77b800b6580b884b48eb3
EchoQuill <safnaj059@gmail.com>
2024-11-14 20:42:36 +0530

    fix: captcha not being detected

--- cogs/captcha.py
+++ cogs/captcha.py
@@ -10,16 +10,17 @@ list_captcha = ["to check that you are a human!","https://owobot.com/captcha","p
 class Captcha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot.log("conf2 - OwO","purple")
+        self.bot.log("conf2 - captcha","purple")
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == self.bot.cm.id and message.author.id == self.bot.owo_bot_id:
+        if message.channel.id == self.bot.dm.id and message.author.id == self.bot.owo_bot_id:
             if "I have verified that you are human! Thank you! :3" in message.content:
                 self.bot.captcha = False
                 self.bot.log(f"captcha solved! - {self.bot.user}", "chartreuse3")
 
-        if message.channel.id == self.bot.dm.id and message.author.id == self.bot.owo_bot_id:
+        if message.channel.id in {self.bot.dm.id, self.bot.cm.id} and message.author.id == self.bot.owo_bot_id:
+            """sets may be faster than list..? maybe.."""
             if any(b in message.content.lower() for b in list_captcha):
                 self.bot.captcha = True
                 self.bot.log(f"captcha detected! - {self.bot.user}", "indian_red")
