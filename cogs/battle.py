09324c3269b6f0d5778c68c77161e4e9746a67fa
EchoQuill <safnaj059@gmail.com>
2024-12-26 17:35:21 +0530

    fix battle continuation dtecting as battle complete

--- cogs/battle.py
+++ cogs/battle.py
@@ -41,19 +41,24 @@ class Battle(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id == self.bot.cm.id and message.author.id == self.bot.owo_bot_id:
-            if message.embeds:
-                for embed in message.embeds:
-                    if embed.author.name is not None and "goes into battle!" in embed.author.name.lower():
-                        self.bot.remove_queue(cmd)
-                        self.bot.log(f"Removed battle from checks from main","cornflower_blue")
-                        await asyncio.sleep(self.bot.random_float(config_dict["commands"]["hunt"]["cooldown"]))
-                        """
-                        self.bot.checks.remove((command, timestamp))
-                        is not used to prevent valueerror and better error management
-                        """
-                        await self.bot.put_queue(cmd)
-                        self.bot.log(f"Added battle to queue again from main","cornflower_blue")
+        if message.reference is not None:
+            return
+        try:
+            if message.channel.id == self.bot.cm.id and message.author.id == self.bot.owo_bot_id:
+                if message.embeds:
+                    for embed in message.embeds:
+                        if embed.author.name is not None and "goes into battle!" in embed.author.name.lower():
+                            self.bot.remove_queue(cmd)
+                            self.bot.log(f"Removed battle from checks from main","cornflower_blue")
+                            await asyncio.sleep(self.bot.random_float(config_dict["commands"]["hunt"]["cooldown"]))
+                            """
+                            self.bot.checks.remove((command, timestamp))
+                            is not used to prevent valueerror and better error management
+                            """
+                            await self.bot.put_queue(cmd)
+                            self.bot.log(f"Added battle to queue again from main","cornflower_blue")
+        except Exception as e:
+            print(e)
                 
                 
 
