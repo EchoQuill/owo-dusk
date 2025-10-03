eadb8f265932da0d3f9a45e5adacbb1178d9e464
EchoQuill <safnaj059@gmail.com>
2025-10-03 14:20:47 +0530

    Fix: HostMode not working, crashing when running on hosting platforms

--- utils/notification.py
+++ utils/notification.py
@@ -1,8 +1,18 @@
+import json
+
 from utils.misc import is_termux, run_system_command
 
 
+with open("config/misc.json", "r") as config_file:
+    misc_dict = json.load(config_file)
+
 def notify(content, title):
+    if misc_dict["hostMode"]:
+        # Notification isn't suppported in hosts and will trigger crash if unhandled
+        return
+    
     on_mobile = is_termux()
+
     if on_mobile:
         run_system_command(
             f"termux-notification -t '{title}' -c '{content}' --led-color '#a575ff' --priority 'high'",
@@ -11,7 +21,6 @@ def notify(content, title):
             )
     else:
         from plyer import notification
-        from playsound3 import playsound
 
         notification.notify(
             title=title,
