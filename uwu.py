ecbf56c4816c98f98a26f6b3bea1d0fdeeb226b2
EchoQuill <92152439+EchoQuill@users.noreply.github.com>
2026-01-20 17:10:51 +0530

    Bump version from 2.3.0 to 2.3.1

--- uwu.py
+++ uwu.py
@@ -118,7 +118,7 @@ owoArt = r"""
  \__/ (_/\_) \__/     (____/\____/(____/(__\_)
 """
 owoPanel = Panel(Align.center(owoArt), style="purple ", highlight=False)
-version = "2.3.0"
+version = "2.3.1"
 
 
 """FLASK APP"""
